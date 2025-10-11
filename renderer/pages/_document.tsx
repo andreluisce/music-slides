7e9f4915536d2bdf3a143e6ed1e7a551468509c3
Andre Evangelista <you@example.com>
2025-10-11 20:35:11 +0100

    feat: add tailwind, shadcn and framer-motion

--- renderer/pages/_document.tsx
+++ renderer/pages/_document.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { css, Global } from '@emotion/react';
 
 class MyDocument extends Document {
   render() {
@@ -9,13 +8,6 @@ class MyDocument extends Document {
         <Head>
           <meta charSet='utf-8' />
         </Head>
-        <Global
-          styles={css`
-            body {
-              font-size: 2.4rem;
-            }
-          `}
-        />
         <body>
           <Main />
           <NextScript />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
-    "./renderer/pages/**/*.{js,ts,jsx,tsx}",
-    "./renderer/components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+    './renderer/pages/**/*.tsx',
+    './renderer/components/**/*.tsx',
+    './renderer/shared/**/*.tsx',
+    './renderer/layouts/**/*.tsx',
+    './renderer/providers/**/*.tsx',
   ],
+  prefix: '',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 }
